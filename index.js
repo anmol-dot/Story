4b345254ccf08434e4df2f1799ba988c80714435
anmol agrawal <anmollalit@icloud.com>
2021-06-20 21:46:48 +0530

    added text 2 and other files

---
+++ index.js
@@ -0,0 +1,4 @@
+document.querySelector("h1").innerHTML="Hey There! This is changed with Javascript"
+document.querySelector("h1").style.backgroundColor="yellow"
+document.querySelector("h1").style.fontSize="3rem"
+
